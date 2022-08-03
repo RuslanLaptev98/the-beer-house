5346b4f411a9901aa45d5eb053cf77de3699657f
ruslanlaptev <RLBlockchain@yandex.ru>
2022-08-04 01:27:01 +0300

    #412: incorrect input bug fix

--- features/BeerCatalog/BeerCatalog.tsx
+++ features/BeerCatalog/BeerCatalog.tsx
@@ -23,40 +23,44 @@ const BeerCatalog: React.FC = () => {
     setBeers();
   }, [setBeers, page, search]);
 
-  if (beers.length > 0) {
-    return (
-      <section className={styles.container}>
-        <SearchBar
-          name='Your favourite beer'
-          search={search}
-          setSearch={setSearch}
-        />
-        <Pagination
-          page={page}
-          increment={incrementPage}
-          decrement={decrementPage}
-          incrementDisabled={beers.length < 12}
-          decrementDisabled={page === 1}
-        />
-        <article className={styles.list}>
-          {beers.map((beer) => (
-            <Link key={beer.id} href={`/beer/${beer.id}`}>
-              <a>
-                <Card beer={beer} />
-              </a>
-            </Link>
-          ))}
-        </article>
-        <Pagination
-          page={page}
-          increment={incrementPage}
-          decrement={decrementPage}
-          incrementDisabled={beers.length < 12}
-          decrementDisabled={page === 1}
-        />
-      </section>
-    );
-  } else return <Loader />;
+  return (
+    <section className={styles.container}>
+      <SearchBar
+        name='Your favourite beer'
+        search={search}
+        setSearch={setSearch}
+      />
+      {beers.length > 0 ? (
+        <>
+          <Pagination
+            page={page}
+            increment={incrementPage}
+            decrement={decrementPage}
+            incrementDisabled={beers.length < 12}
+            decrementDisabled={page === 1}
+          />
+          <article className={styles.list}>
+            {beers.map((beer) => (
+              <Link key={beer.id} href={`/beer/${beer.id}`}>
+                <a>
+                  <Card beer={beer} />
+                </a>
+              </Link>
+            ))}
+          </article>
+          <Pagination
+            page={page}
+            increment={incrementPage}
+            decrement={decrementPage}
+            incrementDisabled={beers.length < 12}
+            decrementDisabled={page === 1}
+          />
+        </>
+      ) : (
+        <p className={styles.notFound}>No beers found :(</p>
+      )}
+    </section>
+  );
 };
 
 export default observer(BeerCatalog);
